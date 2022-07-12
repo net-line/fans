096131aaf9a655e0f974623454ffa149a85fc473
Thalionmel <92447482+Thalionmel@users.noreply.github.com>
2022-07-12 11:15:16 +0200

    hide show messenger

--- src/components/Cards/messengercard.js
+++ src/components/Cards/messengercard.js
@@ -1,11 +1,11 @@
 import MyButton from "./WrapperComponents/myCard.js/myButton";
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import useMyHttp from "../../hooks/myhttp";
 
 const Messengercard=(props)=>{
   
- 
+ const [isHidden, setIsHidden] = useState(true);
   const { isLoading, data, error, sendRequest, clear } = useMyHttp()
   function fetchMessagesHandler() {
     //sendRequest(`https://api.deine.fans/api/timeline?producerID=${props.id}`);
@@ -17,17 +17,22 @@ const Messengercard=(props)=>{
     console.log(data);
     console.log(error);
     console.log(clear);
-  
+   setIsHidden(!isHidden);
   } 
+  useEffect(()=>{
+
+    fetchMessagesHandler()
+   
+  },[]);
  
 return (
   <div>
     <div onClick={fetchMessagesHandler}>
       <MyButton>Messenger:</MyButton>
     </div>
-   {data &&<div>
-      {data && data.posts.map((peops) => <h5 key={peops.id}>{peops.id} </h5>)}
-      {!data && <h5>No Data</h5>}
+   {isHidden &&<div>
+      {data &&data.posts && data.posts.map((peops) => <h5 key={peops.id}>{peops.id} </h5>)}
+    {isLoading&&<h5>Lade Daten</h5>}
       {error && <p>{error}</p>}
     </div>}
   </div>
