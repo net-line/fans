eda90ae1f58028252e100bdccc7aedde7eb7cc3c
Thalionmel <92447482+Thalionmel@users.noreply.github.com>
2022-07-18 13:14:46 +0200

    Update postCard.js

--- src/components/Cards/postCard.js
+++ src/components/Cards/postCard.js
@@ -2,6 +2,7 @@ import React from "react";
 
 import classes from "./postCard.module.css";
 import SmallImageElement from "./smallimageElement";
+import MyButton from "./WrapperComponents/myCard.js/myButton";
 
 const PostCard = (props) =>{
 
@@ -48,28 +49,50 @@ return (
       <div>
         <small>kostenpflichtige Inhalte:</small>
         <div className="d-flex flex-wrap" style={{ gap: "4px" }}>
-          {props.thepost.payMedia.map((item) => (
-            <div key={`a${item.guid}`} className="smallMediaPreview">
-              <div key={`b${item.guid}`} className="smallMediaPreviewIcon">
-                {item.type === "image" ? (
-                  <h5>Bild</h5>
-                ) : item.type === "video" ? (
-                  <h5>Video</h5>
-                ) : item.type === "audio" ? (
-                  <h5>Sound</h5>
-                ) : null}
+          {props.thepost.payMedia.map((item) =>
+            item.isPaid ? (
+              <div key={`a${item.guid}`} className="smallMediaPreview">
+                <div key={`b${item.guid}`} className="smallMediaPreviewIcon">
+                  {item.type === "image" ? (
+                    <h5>Bild </h5>
+                  ) : item.type === "video" ? (
+                    <h5>Video</h5>
+                  ) : item.type === "audio" ? (
+                    <h5>Sound</h5>
+                  ) : null}
+                </div>
+                <div key={`c${item.guid}`} className="w-100">
+                  <SmallImageElement
+                    imgid={item.mediaID}
+                    producerID={props.producerID}
+                    token={props.authToken}
+                    clientExtension=".webp"
+                    dimension="320x160"
+                  />
+                </div>
               </div>
-              <div key={`c${item.guid}`} className="w-100">
-                <SmallImageElement
-                  imgid={item.mediaID}
-                  producerID={props.producerID}
-                  token={props.authToken}
-                  clientExtension=".webp"
-                  dimension="320x160"
-                />
+            ) : (
+                
+              <div key={`ba${item.guid}`}>
+                <div key={`a${item.guid}`} className="smallMediaPreview">
+                <div key={`b${item.guid}`} className="smallMediaPreviewIcon">
+                  {item.type === "image" ? (
+                    <h5>Bild </h5>
+                  ) : item.type === "video" ? (
+                    <h5>Video</h5>
+                  ) : item.type === "audio" ? (
+                    <h5>Sound</h5>
+                  ) : null}
+                </div>
+                </div>
+                {console.log(item.type)}
+                <h5>
+                  Schalte diesen Post für nur {props.thepost.price} EUR frei!
+                </h5>
+                <MyButton key={`bb${item.guid}`}>Kaufs Dir</MyButton>
               </div>
-            </div>
-          ))}
+            )
+          )}
         </div>
       </div>
     ) : null}
