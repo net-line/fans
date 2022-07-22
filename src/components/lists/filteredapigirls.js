972d30bae830523683665e831da6b5ca22ca1375
Thalionmel <92447482+Thalionmel@users.noreply.github.com>
2022-07-22 12:17:15 +0200

    Update filteredapigirls.js

--- src/components/lists/filteredapigirls.js
+++ src/components/lists/filteredapigirls.js
@@ -4,12 +4,14 @@ import { useEffect } from "react";
 import useMyHttp from "../../hooks/myhttp";
 import Modeladvertorial from "../Cards/modeladvertorial";
 import { useParams } from "react-router-dom";
+import AllApiGirls from "./allapigirls";
+import { useTranslation } from "react-i18next";
 
 const FilteredApiGirls = (props) => {
 
 const params=useParams()
 
-
+const {t}=useTranslation();
 
   const { isLoading, data, error, sendRequest } = useMyHttp();
   const [didIRun, setDidIrun] = useState(false)
@@ -35,7 +37,13 @@ const params=useParams()
           <h5>{fetchGirlsHandler2()}</h5>
         )}
         {data && data.girls.length < 1 && didIRun && (
-          <h5>Leider keine passenden Girls gefunden</h5>
+          <div>
+            <h5>
+              {t("notfound")} {t("tryoneofthese")}
+            </h5>
+            <AllApiGirls />
+            
+          </div>
         )}
         {data &&
           data.girls &&
